6f860c5a157892b2b71b7e7566ee3413aefc6061
Adwet <adwetanurag.dash2021@vitstudent.ac.in>
2023-06-03 01:00:52 +0530

    added props in components

--- src/MyComponents/ExpenseItem.js
+++ src/MyComponents/ExpenseItem.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const date = new Date();
-    const currentDate = date.getDate(); 
-    const description = "car Insurance";  
-    const expense = "$500";  
+function ExpenseItem(props) {
+    // const date = new Date();
+    // const currentDate = date.getDate(); 
+    // const description = "car Insurance";  
+    // const expense = "$500";  
     
     return (
       <div className='expense-item' >
-        <div>{currentDate}</div>
+        <div>{props.date.toISOString()}</div>
         <div className='expense-item__description' >
-          <h2>{description}</h2>
-          <div className='expense-item__price'>{expense}</div>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>{props.amount}</div>
         </div>
       </div>
     );
