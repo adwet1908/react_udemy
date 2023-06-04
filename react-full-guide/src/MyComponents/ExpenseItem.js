4c619d047ef4dc0e84ab6c95f3d43f6553750359
Adwet <adwetanurag.dash2021@vitstudent.ac.in>
2023-06-04 19:50:47 +0530

    used first react-hook

--- react-full-guide/src/MyComponents/ExpenseItem.js
+++ react-full-guide/src/MyComponents/ExpenseItem.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './ExpenseItem.css'
 import './ExpenseDate'
 import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title)
+  const clickHandler = ()=>{
+    setTitle("Updated"); 
+    console.log(title)     
+  }
   return (
     <>
-      <div className='expense-item'>
+      <Card className='expense-item'>
       <ExpenseDate date = {props.date}/>
         <div className='expense-item__description' >
           <h2>{props.title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
         </div>
-      </div>
+        <button onClick = {clickHandler}>Button</button>
+      </Card> 
       </>
   );
 }
